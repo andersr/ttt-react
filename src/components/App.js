1e1194198b52c4338674213332af4e3f0a22464d
Anders Ramsay <anders@gmail.com>
2016-12-29 12:36:47 -0500

    app test passing

---
+++ src/components/App.js
@@ -0,0 +1,6 @@
+import React from 'react'
+import SelectPlayer from './SelectPlayer'
+
+const App = () => <div><SelectPlayer /></div>
+
+export default App
---
+++ src/components/SelectPlayer.js
@@ -0,0 +1,5 @@
+import React from 'react'
+
+const SelectPlayer = () => <div>Select Player</div>
+
+export default SelectPlayer
